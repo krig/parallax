989ebba32ed05d0d94861b3062ee32d0af4aa6b8
Valentin Vidic <Valentin.Vidic@CARNet.hr>
2016-11-14 16:48:37 +0100

    Fix module failure with python3
    signal.set_wakeup_fd(wakeup_writefd)
    ValueError: the fd 4 must be in non-blocking mode

--- parallax/manager.py
+++ parallax/manager.py
@@ -8,6 +8,7 @@ import signal
 import sys
 import threading
 import copy
+import fcntl
 
 try:
     import queue
@@ -247,6 +248,7 @@ class IOMap(object):
 
         # Setup the wakeup file descriptor to avoid hanging on lost signals.
         wakeup_readfd, wakeup_writefd = os.pipe()
+        fcntl.fcntl(wakeup_writefd, fcntl.F_SETFL, os.O_NONBLOCK)
         self.register_read(wakeup_readfd, self.wakeup_handler)
         # TODO: remove test when we stop supporting Python <2.5
         if hasattr(signal, 'set_wakeup_fd'):
